6351177972fa77c5d03680f5ecf8da378c19637e
Oliver Bath <oliverbth05@gmail.com>
2019-01-18 02:10:00 +0000

    Browser fix

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const path = require('path');
+const browser = require('browser-detect');
 
 const app = express();
+
 
 app.use(express.static(path.join(__dirname, 'static')))
 
 app.get('/*', (req, res) => {
-    console.log('recived')
-    res.sendFile(path.join(__dirname, 'index.html'))
+    if (browser(req.headers['user-agent']).name === 'ie') {
+        res.sendFile(path.join(__dirname, 'ie.html'))
+    }
+    else {
+        res.sendFile(path.join(__dirname, 'index.html'))
+    }
+    
 })
 
 app.listen(process.env.PORT || 3000, process.env.IP, () => {
